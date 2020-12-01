bb544d7002c7975bddffd4f439b0f96a42605629
xXDaniel123 <daniel91dante@gmail.com>
2020-12-01 14:48:58 +0200

    try last time

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ const genreRoute = require('./api/genre/genre.routes.js')
 
 
 if (process.env.NODE_ENV === 'production') {
-    // app.use(express.static(path.resolve(__dirname, 'public')));
-    pp.use(express.static('public'));
+    app.use(express.static(path.resolve(__dirname, 'public')));
+    // pp.use(express.static('public'));
 } else {
     const corsOptions = {
         origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000'],
