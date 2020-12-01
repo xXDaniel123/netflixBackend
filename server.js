571770a8e66d8574252626a98bf886bc25fbbcc4
xXDaniel123 <daniel91dante@gmail.com>
2020-12-01 03:18:01 +0200

    added dbUrl

--- server.js
+++ server.js
@@ -48,5 +48,5 @@ app.get('/**', (req, res) => {
 })
 
 // Listening to the port
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3030;
 app.listen(port, () => console.log('server is up'))
