344b479e98f6cfd083f809acfe57802e13d96de0
Daniel Dante <daniel91dante@gmail.com>
2020-12-04 01:15:38 +0200

    added support for mobile and responsive design

--- key.js
+++
@@ -1,8 +0,0 @@
-// add it to git ignore
-
-module.exports = {
-    google: {
-        clientID: '168447752977-topi06sp3n60pk37e8j5dg91lnjophvi.apps.googleusercontent.com',
-        clientSSecret: 'i6R5cIxsf0y-nxL37fMee7-Z'
-    }
-}
